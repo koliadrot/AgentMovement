d6867363d894657d89bf97838041c1452299037c
DESKTOP-8O0TOOS\blazz <koliadrott@gmail.com>
2022-08-04 00:20:49 +0200

    Вынес отдельно реализацию  Strafe

---
+++ Assets/AgentMovement/Scripts/Strafe.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace AgentMovement
+{
+    /// <summary>
+    /// Тип движения "strafe"
+    /// </summary>
+    public class Strafe : MonoBehaviour
+    {
+        private bool isStrafe;
+
+        private void Update()
+        {
+            //TODO:Нужна обертка для инпута
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                isStrafe = !isStrafe;
+            }
+            StrafeMovement();
+        }
+
+        private void StrafeMovement()
+        {
+            if (isStrafe)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0f, transform.eulerAngles.z);
+            }
+        }
+    }
+}
